e75f9b030b2b5bec0331eb9d3383fbaec23f02de
schakravarti <schakravarti930@gmail.com>
2020-10-13 09:45:14 +0530

    [Swapnil] Add : Create A New Game Board

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame.CreateBoard();
+            Console.WriteLine(TicTacToeGame.Board);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class TicTacToeGame
+    {
+        public static char[] Board;
+        public static void CreateBoard()
+        {
+            Board = new char[10];
+            for(int i = 0; i < Board.Length; i++)
+            {
+                Board[i] = ' ';
+            }
+        }
+    }
+}
